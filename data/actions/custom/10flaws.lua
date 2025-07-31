d90511219196f29047155bce665bc039eadb860b
Johnspartan22 <109169625+Johnspartan22@users.noreply.github.com>
2025-07-31 13:36:10 -0400

    Delete data/actions/custom directory
    removed the wrong directory

--- data/actions/custom/10flaws.lua
+++
@@ -1,8 +0,0 @@
-function onUse(cid, item, fromPosition, itemEx, toPosition)
-     if getPlayerStorageValue(cid, 59852) == -1 then
-          setPlayerStorageValue(cid, 59852, 1)
-          doPlayerAddItem(cid, 2229, 10)
-	doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "You found 10 flawless skulls")
-     end
-     return TRUE
-end
--- data/actions/custom/50tokens.lua
+++
@@ -1,8 +0,0 @@
-function onUse(cid, item, fromPosition, itemEx, toPosition)
-     if getPlayerStorageValue(cid, 59852) == -1 then
-          setPlayerStorageValue(cid, 59852, 1)
-          doPlayerAddItem(cid, 2151, 50)
-	doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "You found 50 Underworld Tokens")
-     end
-     return TRUE
-end
